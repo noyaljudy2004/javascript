ed5674a994c4cf39f6704bacd41e068f077b8cd7
Noyal <noyaljudy1@gmail.com>
2025-07-18 09:05:30 +0530

    javascript

---
+++ basic.js
@@ -0,0 +1,15 @@
+console.log("this javasript");
+
+console.log(1);
+console.log(1.56);
+console.log("e");
+console.log("java");
+
+console.log(typeof 1);
+console.log(typeof 1.56);
+console.log(typeof "e");
+console.log(typeof "java");
+
+console.log(block);
+const block=1
+

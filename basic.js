8c10829ab4dcc739216d24dbd1042d538b042e23
Noyal <noyaljudy1@gmail.com>
2025-07-18 11:01:08 +0530

    js for javascript

--- basic.js
+++ basic.js
@@ -1,15 +1,31 @@
-console.log("this javasript");
+// console.log("this javasript");
 
-console.log(1);
-console.log(1.56);
-console.log("e");
-console.log("java");
+// console.log(1);
+// console.log(1.56);
+// console.log("e");
+// console.log("java");
 
-console.log(typeof 1);
-console.log(typeof 1.56);
-console.log(typeof "e");
-console.log(typeof "java");
+// console.log(typeof 1);
+// console.log(typeof 1.56);
+// console.log(typeof "e");
+// console.log(typeof "java");
 
-console.log(block);
-const block=1
+// console.log(block);
+// const block=1
+// num=23
+
+
+// console.log(num>=10 && num<=15);
+
+
+a=20
+b=10
+//a=a+b
+// b=a-b
+// a=a-b
+c=a
+a=b
+b=c
+console.log("a:",a);
+console.log("b:",b);
 
---
+++ js2.js
@@ -0,0 +1,44 @@
+// const company="XYZ PVT"
+// const location="calicut"
+// const zipcode=673500
+// console.log("the",company,"located at",location,"and zipcode is",zipcode);
+// console.log("the "+company+"located at "+location+"and zipcode is "+zipcode);
+// console.log(`the company ${company} located at ${location} and zipcode is ${zipcode}`);
+
+// age=19
+// if(age>=18){
+//     console.log("eligible for voting");
+    
+// }
+// else{
+//     console.log("Not eligible for voting");
+    
+// }
+
+// number=0
+// if(number>0){
+//     console.log("+ve number");
+    
+// }
+// else if(number<0){
+//     console.log("-ve number");
+    
+// }
+// else{
+//     console.log("Zero");
+    
+// }
+num1=20
+num2=20
+if(num1>num2){
+    console.log(`${num1} is a larger than ${num2}`);
+    
+}
+else if(num1<num2){
+    console.log(`${num2} is a larger than ${num1}`);
+    
+}
+else{
+    console.log("both are equal");
+    
+}

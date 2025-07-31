29673620aa09069f01e6a7fd824085fe66761adf
Noyal <noyaljudy1@gmail.com>
2025-07-31 10:57:09 +0530

    array for javascript

---
+++ variable.js
@@ -0,0 +1,128 @@
+// // monday to friday
+// const days=["monday","tuesday","wednesday","thursday","friday","saturday"]
+// console.log(days);
+// console.log(typeof days);
+// console.log(days[5]);
+// console.log(days.length);
+// console.log(days.length-1);
+// for(i=0;i<days.length;i++){
+//     console.log(days[i]);
+    
+// }
+// // for of loop
+// console.log("======================");
+// for(let day of days){
+//     console.log(day);
+    
+// }
+// // for in-loop
+// console.log("------------------------");
+// for(let index in days){
+//     console.log(days[index]);
+    
+// }
+// largest num
+
+//  let numbers=[10,2,13,33,24,19]
+// let largest = Math.max(...numbers);
+// console.log("Largest number is:", largest);{
+
+    
+    
+
+// let numbers=[10,2,13,33,24,19]
+// let smallest = Math.min(...numbers);
+// console.log("smallest number is:", smallest);{
+
+    
+    
+// }
+// miss
+// numArray=[10,2,13,33,24,19]
+// largest=numArray[0]
+// for(let num of numArray){
+//     if(num>largest){
+//         largest=num
+//     }
+// }
+// console.log(`largest number=${largest}`);
+
+// small
+// numArray=[10,2,13,33,24,19]
+// smallest=numArray[0]
+// for(let num of numArray){
+//     if(num<smallest){
+//         smallest=num
+//     }
+// }
+// console.log(`smallest number=${smallest}`);
+
+
+// sum
+
+// let sum=0
+// for(let num of numArray){
+//     sum=sum+num
+// }
+// console.log(sum);
+// to check number in array
+//  numArray=[10,2,13,33,24,19]
+//  isarray=true//flag operator
+//  for(let num of numArray)
+// {
+//     if(num==23){
+//         isarray=true
+
+//     }
+//     else{
+//         isarray=false
+//     }
+
+// }
+// console.log(isarray?"number 23 is present in this array":"number 23 is not present in this array");
+
+
+// const days=["monday","tuesday","wednesday","thursday","friday","saturday"]
+// days.push("sunday")
+// console.log(days);//last
+// days.unshift("sunday")
+// console.log(days);//first
+// days.pop()//first
+//     console.log(days);
+//     days.shift()//last
+//     console.log(days
+
+//     );
+//print the pattern
+input=[4,5,6]
+output=[]
+sum=0
+// output=[11,10,9]
+for(let num of input){
+    sum=sum+num
+
+}
+for(let num of input){
+    output.push(sum-num)
+}
+console.log(output);
+
+//print duplicate number
+duplicateArray=[10,29,40,20,10,11,23]
+
+//print the pairs whose sum=7
+pairSumArray=[2,3,4,5]
+
+//print all common numbers
+p=[10,22,12,20,30]
+q=[11,20,21,30,31]
+
+    
+
+
+
+
+
+
+
+

082bc2ed11c45cfb2609a7f7f2868554dd736083
Noyal <noyaljudy1@gmail.com>
2025-08-22 10:21:29 +0530

    kxehjexd

---
+++ calcc.js
@@ -0,0 +1,39 @@
+const displayData=(data)=>{
+    const display = document.getElementById('output')
+    display.value+=data
+}
+const allClear=()=>{
+    const display = document.getElementById('output')
+    display.value =""
+    display.placeholder = 0
+}
+// clear last character
+const clearOne = () => {
+  const ot = document.getElementById('output');
+  ot.value = ot.value.slice(0, -1);
+};
+const Dlt=()=>{
+     const display = document.getElementById('output');
+    display.value=eval(display.value)
+    
+
+}
+const Result=()=>{
+     const display = document.getElementById('output');
+    try{
+       ;
+        display.value = eval(display.value)
+        
+    }
+    catch(err){
+     
+        display.value=""
+        display.placeholder="Invalid Experssion!!"
+        
+    }
+    finally{
+        console.log("task completed");
+        
+    }
+    
+}
